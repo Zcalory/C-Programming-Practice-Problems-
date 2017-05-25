1479c2c4fef7bfeabf5f52b62099876e03eef89e
Zcalory <zcalory@hotmail.com>
2017-05-25 10:33:11 +0800

    Update Linked list.cpp
    finish Delete and Insert

--- Data Structure/Linked list.cpp
+++ Data Structure/Linked list.cpp
@@ -11,11 +11,16 @@ struct Node
 typedef struct Node *head;
 typedef head List;
 typedef head Last;
+typedef head Position;
 
+List CreatList();
 List MakeEmpty(List L);
 bool IsEmpty(List L);
-void Add(int value, List L);
-List CreatList();
+void Add(int value, List &L);
+void Insert(int value, Position p, List L);
+void Delete(int value, List L);
+Position GetPosition(int value, List L);
+Position GetPreviousPosition(int value, List L);
 void ShowList(List L);
 Last FindListLast(List L);
 
@@ -24,7 +29,16 @@ Last FindListLast(List L);
 
 int main() {
 	List L=CreatList();
-	Add(6, L);
+	Add(2, L);
+	Add(3, L);
+	Add(4, L);
+	ShowList(L);
+	Insert(8, GetPosition(4, L), L);
+	Insert(9, GetPosition(8, L), L);
+	ShowList(L);
+	Delete(3, L);
+	ShowList(L);
+	L=MakeEmpty(L);
 	ShowList(L);
 	system("pause");
 }
@@ -52,14 +66,18 @@ void ShowList(List L) {
 
 //Find the last element of the list
 Last FindListLast(List L) {
-	if (L->next == NULL)
-		return L;
-	else L = L->next;
+	while (L)
+	{
+		if (L->next == NULL)
+			return L;
+		else L = L->next;
+	}
+
 }
 
 //Add one element at the end of the linked list
 //value id the data of that element
-void Add(int value, List L) {
+void Add(int value, List &L) {
 	Node *new_n;
 	new_n = new Node;
 	new_n->data = value;
@@ -78,7 +96,7 @@ bool IsEmpty(List L) {
 
 //Make the list empty
 List MakeEmpty(List L) {
-	List tem = L,tem2;
+	List tem = L->next,tem2;
 	while (tem) {
 		tem2 = tem;
 		tem = tem->next;
@@ -86,4 +104,50 @@ List MakeEmpty(List L) {
 	}
 	L ->next = NULL;
 	return L;
+}
+
+//Get the position by the value
+//if the value is not contained in the list,return NULL
+Position GetPosition(int value, List L) {
+	while (L) {
+		if (L->data == value)
+			return L;
+		L = L->next;
+	}
+	cout << "Cannot find "<<value<<"in the list." << endl;
+	return NULL;
+}
+
+//Get the previous element positon by the value
+//if the value is not contained in the list,return NULL
+Position GetPreviousPosition(int value, List L) {
+	while(L){
+		if (L->next != NULL&&L->next->data == value)
+			return L;
+		else L = L->next;
+	}
+		cout << "Cannot find "<<value<<"in the list." << endl;
+	return NULL;
+}
+
+//Insert an element after p
+void Insert(int value, Position p, List L) {
+	if (p == NULL)
+		cout << "Invalid position,error." << endl;
+	else
+	{
+		Node *n;
+		n = new Node;
+		n->data = value;
+		n->next = p->next;
+		p ->next = n;
+	}
+}
+
+//Delete a element from the list
+void Delete(int value, List L) {
+	Node *n=GetPosition(value,L),
+		*m=GetPreviousPosition(value,L);
+	m->next = n->next;
+	delete n;
 }
